4b2d6af90e137db75e38e51c7d8894ec28b24eda
Aman <asingh59592@gmail.com>
2024-08-17 18:00:36 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 11
+let accountEmail = "sdcfdc123@gmail.com"
+var accountPassword = "12345"
+accountCity = "pakistan"
+let accountState;
+
+// accountId = 2
+accountEmail = "scdef@gmail.com"
+accountPassword = "2233554"
+accountCity = "lahore"
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/datatypes.js
@@ -0,0 +1,25 @@
+"use strict";  //it s use to treat all code as newer version it is not mandatory bcz now all program already runs on strict mode or in newer version
+
+//alert(3+3)
+//we are using node js not browser
+
+console.log(3+3);
+console.log("sfvsf");
+
+let name = "dsbrkg";
+let age = 20
+let isLoggedIn = false
+let state = null
+
+// number => 2 to power 53
+//bigint
+//string=>""
+//boolean => true/false
+//null => standalonevalue
+//undefined =>
+//symbol => unique
+
+//object
+console.log(typeof"vsfvc")
+console.log(typeof undefined) //undefined it is a type
+console.log(typeof null)    // its is object in js
