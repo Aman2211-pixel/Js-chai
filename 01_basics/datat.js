c2f2cd9d39e67338f427460023e804d09daef058
Aman <asingh59592@gmail.com>
2024-08-18 11:46:54 +0000

    container config done

---
+++ 01_basics/datat.js
@@ -0,0 +1,44 @@
+// Primitive
+
+// 7 type : String,Number,Boolean, null ,undefined , Symbol, BigInt
+
+//Reference (Non Primitive)
+
+//Array ,Objects ,Functions
+
+const score =100
+const scoreValue = 100.3
+
+const isLoggedIn= false
+const outsideTemp = null
+let userEmail
+
+const id = Symbol('123')
+const anotherId=Symbol('123')
+
+console.log(id===anotherId);
+const bigNumber  =3151302315516522233n
+
+
+
+const heros = ["shaktiman","nagraj","chotabheem"]
+
+let myObj={
+name:"aman",
+age:22,
+}
+
+const myFunction = function(){
+    console.log("Hellow world");
+  
+}
+
+console.log(typeof bigNumber);
+console.log(typeof outsideTemp);
+console.log(typeof scoreValue);
+console.log(typeof myFunction);
+console.log(typeof heros);
+console.log(typeof anotherId);
+
+
+
